0055f0b4ea7419e728f60a0d5e1e50465adc15d4
Aiden Kim <aidengookjinkim@gmail.com>
2021-01-19 22:48:15 -0700

    Conneting to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,15 +1,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import config from './config';
 
 const app = express();
+const { MONGO_URI } = config;
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cors());
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('MongoDB connected!'))
+  .catch(err => {
+    throw err;
+  });
 
-// app.get('/', (req, res) => {
-//   res.send('HI');
-// });
+app.get('/', (req, res) => {
+  res.send('HI');
+});
 
 export default app;
