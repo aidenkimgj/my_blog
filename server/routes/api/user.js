48328501fe4deb122b14dd842f26b6a03c0a8b8a
Aiden Kim <aidengookjinkim@gmail.com>
2021-01-22 00:29:58 -0700

    Create UserRoutes

---
+++ server/routes/api/user.js
@@ -0,0 +1,78 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from '../../config/index';
+const { JWT_SECRET } = config;
+
+// Model
+import User from '../../models/user';
+
+const router = express.Router();
+
+// @routes     GET api/user
+// @desc       get all user
+// @access     public
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+
+    if (!user) throw Error('No users');
+    res.status(200).json(users);
+  } catch (e) {
+    console.log(e);
+    // 만약에 에러가 나오면 400 코드를 보내서 실패 했다고 알려줌 json을 사용하여 메시지를 담아 보낸다.
+    res.status(400).json({ msg: e.message });
+  }
+});
+
+// @routes     POST api/user
+// @desc       Register user
+// @access     public
+
+router.post('/', (req, res) => {
+  console.log(req);
+  const { name, email, password } = req.body;
+
+  // Simple validation
+  if (!name || !email || !password) {
+    return res.status(400).json({ msg: 'Please fill up all field!!' });
+  }
+
+  // Check for exising user
+  User.findOne({ email }).then(user => {
+    if (user) return res.status(400).json({ msg: 'User already exists!!' });
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
+    // 해쉬값을 만들어 줌
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser.save().then(user => {
+          jwt.sign(
+            { id: user.id },
+            JWT_SECRET,
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) throw err;
+              res.json({
+                token,
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email,
+                },
+              });
+            }
+          );
+        });
+      });
+    });
+  });
+});
+
+export default router;
