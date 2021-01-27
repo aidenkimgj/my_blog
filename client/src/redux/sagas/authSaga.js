1572c12e76f5d7101823541fe3cab25b7988832f
Aiden Kim <aidengookjinkim@gmail.com>
2021-01-26 22:59:29 -0700

    Add logout into authSaga.js

--- client/src/redux/sagas/authSaga.js
+++ client/src/redux/sagas/authSaga.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 import { all, call, put, takeEvery, fork } from 'redux-saga/effects';
-import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS } from '../types';
+import {
+  LOGIN_FAILURE,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGOUT_FAILURE,
+  LOGOUT_REQUEST,
+  LOGOUT_SUCCESS,
+} from '../types';
 
 // Login User API
 
@@ -17,8 +24,10 @@ const loginUserAPI = loginData => {
 
 function* loginUser(action) {
   try {
+    // call() 함수를 동기적으로 실행 call에 넘겨진 함수가 Promise를 리턴하면 그 Promise가 resolved 될 때까지 call()을 호출한 부분에서 실행이 멈춘다
     const result = yield call(loginUserAPI, action.payload);
     console.log(result);
+    // put() 액션을 dispatch 한다 보통 take로 액션을 캐치해서 api 호출을 call로 실행하고 성공/실패 여부에 따라 리덕스 store에 반영하기 위해서 호출하는 effect임
     yield put({
       type: LOGIN_SUCCESS,
       payload: result.data,
@@ -30,11 +39,32 @@ function* loginUser(action) {
     });
   }
 }
-// 매번 로그인 요청을 보고 있으라는 함수
+// takeEvery() 캐치된 모든 액션에 대해서 핸들러를 실행한다 (매번 로그인 요청을 보고 있으라는 함수)
 function* watchLoginUser() {
   yield takeEvery(LOGIN_REQUEST, loginUser);
 }
 
+// Logout User
+
+function* logoutUser(action) {
+  try {
+    // put() 액션을 dispatch 한다 보통 take로 액션을 캐치해서 api 호출을 call로 실행하고 성공/실패 여부에 따라 리덕스 store에 반영하기 위해서 호출하는 effect임
+    yield put({
+      type: LOGOUT_SUCCESS,
+    });
+  } catch (e) {
+    yield put({
+      type: LOGOUT_FAILURE,
+    });
+    console.log(e);
+  }
+}
+// takeEvery() 캐치된 모든 액션에 대해서 핸들러를 실행한다 (매번 로그아웃 요청을 보고 있으라는 함수)
+function* watchLogoutUser() {
+  yield takeEvery(LOGOUT_REQUEST, logoutUser);
+}
+
 export default function* authSaga() {
-  yield all([fork(watchLoginUser)]);
+  // fork() 매개변수로 전달된 함수를 비동기적으로 실행
+  yield all([fork(watchLoginUser), fork(watchLogoutUser)]);
 }
