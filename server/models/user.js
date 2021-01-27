939d2684847423c3850c0a2b92a9b71475455ba9
Aiden Kim <aidengookjinkim@gmail.com>
2021-01-27 00:29:12 -0700

    Change role name

--- server/models/user.js
+++ server/models/user.js
@@ -19,7 +19,7 @@ const UserSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['MainJuin', 'SubJuin', 'User'],
+    enum: ['Owner', 'SubOwner', 'User'],
     default: 'User',
   },
   register_date: {
