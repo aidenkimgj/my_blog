854f253eab24edb96c75d1f1d3fc91e38b530543
Aiden Kim <aidengookjinkim@gmail.com>
2021-01-30 15:11:03 -0700

    Add several types for redux

--- client/src/redux/types.js
+++ client/src/redux/types.js
@@ -1,24 +1,69 @@
-// Login
+// Login type for authReducer
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
-// Logout
+// Logout type for authReducer
 export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
 export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
 
-// User Loading
+// User Loading type for authReducer
 export const USER_LOADING_REQUEST = 'USER_LOADING_REQUEST';
 export const USER_LOADING_SUCCESS = 'USER_LOADING_SUCCESS';
 export const USER_LOADING_FAILURE = 'USER_LOADING_FAILURE';
 
-// Register
+// Register type for authReducer
 export const REGISTER_REQUEST = 'REGISTER_REQUEST';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAILURE = 'REGISTER_FAILURE';
 
-// Clear Error
+// Clear Error type for authReducer
 export const CLEAR_ERROR_REQUEST = 'CLEAR_ERROR_REQUEST';
 export const CLEAR_ERROR_SUCCESS = 'CLEAR_ERROR_SUCCESS';
 export const CLEAR_ERROR_FAILURE = 'CLEAR_ERROR_FAILURE';
+
+// Post loading type for postReducer
+export const POST_LOADING_REQUEST = 'POST_LOADING_REQUEST';
+export const POST_LOADING_SUCCESS = 'POST_LOADING_SUCCESS';
+export const POST_LOADING_FAILURE = 'POST_LOADING_FAILURE';
+
+// Post write type for postReducer
+export const POST_WRITE_REQUEST = 'POST_WRITE_REQUEST';
+export const POST_WRITE_FAILURE = 'POST_WRITE_FAILURE';
+export const POST_WRITE_SUCCESS = 'POST_WRITE_SUCCESS';
+
+// Post upload type for postReducer
+export const POST_UPLOADING_REQUEST = 'POST_UPLOADING_REQUEST';
+export const POST_UPLOADING_FAILURE = 'POST_UPLOADING_FAILURE';
+export const POST_UPLOADING_SUCCESS = 'POST_UPLOADING_SUCCESS';
+
+// Post Detail loading type for postReducer
+export const POST_DETAIL_LOADING_REQUEST = 'POST_DETAIL_LOADING_REQUEST';
+export const POST_DETAIL_LOADING_FAILURE = 'POST_DETAIL_LOADING_FAILURE';
+export const POST_DETAIL_LOADING_SUCCESS = 'POST_DETAIL_LOADING_SUCCESS';
+
+// Post Delete type for postReducer
+export const POST_DELETE_REQUEST = 'POST_DELETE_REQUEST';
+export const POST_DELETE_FAILURE = 'POST_DELETE_FAILURE';
+export const POST_DELETE_SUCCESS = 'POST_DELETE_SUCCESS';
+
+// Post Edit loading type for postReducer
+export const POST_EDIT_LOADING_REQUEST = 'POST_EDIT_LOADING_REQUEST';
+export const POST_EDIT_LOADING_FAILURE = 'POST_EDIT_LOADING_FAILURE';
+export const POST_EDIT_LOADING_SUCCESS = 'POST_EDIT_LOADING_SUCCESS';
+
+// Post Edit uploading type for postReducer
+export const POST_EDIT_UPLOADING_REQUEST = 'POST_EDIT_UPLOADING_REQUEST';
+export const POST_EDIT_UPLOADING_FAILURE = 'POST_EDIT_UPLOADING_FAILURE';
+export const POST_EDIT_UPLOADING_SUCCESS = 'POST_EDIT_UPLOADING_SUCCESS';
+
+// Category Find type for postReducer
+export const CATEGORY_FIND_REQUEST = 'CATEGORY_FIND_REQUEST';
+export const CATEGORY_FIND_FAILURE = 'CATEGORY_FIND_FAILURE';
+export const CATEGORY_FIND_SUCCESS = 'CATEGORY_FIND_SUCCESS';
+
+// Search type for postReducer
+export const SEARCH_REQUEST = 'SEARCH_REQUEST';
+export const SEARCH_FAILURE = 'SEARCH_FAILURE';
+export const SEARCH_SUCCESS = 'SEARCH_SUCCESS';
