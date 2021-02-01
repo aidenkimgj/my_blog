120d56c2b59fb2e3bf0ddbd61938abb1925fd23e
Aiden Kim <aidengookjinkim@gmail.com>
2021-02-01 14:40:04 -0700

    Create image router

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -6,6 +6,51 @@ import Post from '../../models/post';
 
 const router = express.Router();
 
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import path from 'path';
+import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_PRIVATE_KEY,
+});
+
+const uploadS3 = multer({
+  storage: multerS3({
+    s3,
+    bucket: 'blog/upload',
+    region: 'us-west-1',
+    // 중복되는 파일의 이름을 구분하기 위해서
+    key(req, file, cb) {
+      const ext = path.extname(file.originalname);
+      const basename = path.basename(file.originalname, ext);
+      cb(null, basename + new Date().valueOf() + ext);
+    },
+  }),
+  // 파일 사이즈 100mb
+  limits: { fileSize: 100 * 1024 * 1024 },
+});
+
+/*
+ * @route     POST   api/post/image
+ * @desc      Create a post
+ * @access    Private
+ */
+
+router.post('/image', uploadS3.array('upload', 5), async (req, res, next) => {
+  try {
+    console.log(req.files.map(v => v.location));
+    res.json({ uploaded: true, url: req.files.map(v => v.location) });
+  } catch (e) {
+    console.error(e);
+    res.json({ uploaded: false, url: null });
+  }
+});
+
 // api/post
 router.get('/', async (req, res) => {
   const postFindResult = await Post.find();
