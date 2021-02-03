eb928edd4c95f20a0e9c9874083b67a329bebbc5
Aiden Kim <aidengookjinkim@gmail.com>
2021-02-03 14:15:18 -0700

    Update post router

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -3,6 +3,8 @@ import auth from '../../middleware/auth';
 
 // Model
 import Post from '../../models/post';
+import Category from '../../models/category';
+import User from '../../models/user';
 
 const router = express.Router();
 
@@ -11,6 +13,7 @@ import multerS3 from 'multer-s3';
 import path from 'path';
 import AWS from 'aws-sdk';
 import dotenv from 'dotenv';
+import { isNullOrUndefined } from 'util';
 
 dotenv.config();
 
@@ -22,7 +25,7 @@ const s3 = new AWS.S3({
 const uploadS3 = multer({
   storage: multerS3({
     s3,
-    bucket: 'blog/upload',
+    bucket: 'blogaiden/upload',
     region: 'us-west-1',
     // 중복되는 파일의 이름을 구분하기 위해서
     key(req, file, cb) {
@@ -59,17 +62,61 @@ router.get('/', async (req, res) => {
   res.json(postFindResult);
 });
 
-router.post('/', auth, async (req, res, next) => {
+/*
+ * @route     POST   api/post/
+ * @desc      Create a post
+ * @access    Private
+ */
+router.post('/', auth, uploadS3.none(), async (req, res, next) => {
   try {
     console.log(req, 'req');
-    const { title, contents, fileUrl, creator } = req.body;
+    const { title, contents, fileUrl, creator, category } = req.body;
     const newPost = await Post.create({
       title,
       contents,
       fileUrl,
       creator,
+      date: moment().format('MM-DD-YYYY hh:mm:ss'),
     });
-    res.json(newPost);
+
+    // find category from database
+    const existedCategory = await Category.findOne({
+      categoryName: category,
+    });
+
+    console.log(existedCategory, 'Find Result category');
+    // the category does not exist in the database.
+    if (isNullOrUndefined(existedCategory)) {
+      // create new category
+      const newCategory = await Category.create({
+        categoryName: category,
+      });
+      // insert data into database
+      await Post.findByIdAndUpdate(newPost._id, {
+        // $push는 기존 배열에 값을 추가해서 넣을때를 의미
+        $push: { category: newCategory._id },
+      });
+      await Category.findByIdAndUpdate(newCategory._id, {
+        $push: { posts: newPost._id },
+      });
+      await User.findByIdAndUpdate(req.user.id, {
+        $push: { posts: newPost._id },
+      });
+
+      // the category exist in the database
+    } else {
+      await Post.findByIdAndUpdate(newPost._id, {
+        // 포스트 모델 입장에서는 특정 포스트 모델에서 카테고리를 찾았으니 이를 업데이트만 해주면  $push가 사용되지 않은 것이다
+        category: existedCategory._id,
+      });
+      await Category.findByIdAndUpdate(existedCategory._id, {
+        $push: { posts: newPost._id },
+      });
+      await User.findByIdAndUpdate(req.user.id, {
+        $push: { posts: newPost._id },
+      });
+    }
+    return res.redirect(`/api/post/${newPost._id}`);
   } catch (e) {
     console.log(e);
   }
