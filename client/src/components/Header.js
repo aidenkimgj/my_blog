de32384e9f69aaeb192bed42177bd53d145de27c
Aiden Kim <aidengookjinkim@gmail.com>
2021-02-15 13:45:01 -0700

    Implement auto typing in Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Col, Row } from 'reactstrap';
+import Typewriter from 'typewriter-effect';
 
 const Header = () => {
   return (
     <div id="page-header" className="mb-3">
       <Row>
         <Col md="6" sm="auto" className="text-center m-auto">
-          <h1>Read My Blog</h1>
-          <p>This is Aiden's Blog</p>
+          <h1>Aiden's Blog</h1>
+          <Typewriter
+            id="type"
+            options={{
+              strings: [
+                'Hello',
+                "I'm Aiden who dreams of becoming an awesome developer in Calgary",
+                'Enjoy my blog',
+              ],
+              autoStart: true,
+              loop: true,
+              delay: 50,
+            }}
+          />
+          {/* <p>I'm Aiden who dreams of becoming a great developer in Calgary.</p> */}
         </Col>
       </Row>
     </div>
