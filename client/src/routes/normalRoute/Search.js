6887892126b2884d695ad9894fca81b8ca9884bc
Aiden Kim <aidengookjinkim@gmail.com>
2021-02-14 19:08:09 -0700

    Create Search.js

--- client/src/routes/normalRoute/Search.js
+++ client/src/routes/normalRoute/Search.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { SEARCH_REQUEST } from '../../redux/types';
+import { Row } from 'reactstrap';
+import PostCardOne from '../../components/post/PostCardOne';
 
 const Search = () => {
-  return <h1>Search</h1>;
+  const dispatch = useDispatch();
+  let { searchTerm } = useParams();
+  const { searchResult } = useSelector(state => state.post);
+
+  useEffect(() => {
+    if (searchTerm) {
+      dispatch({
+        type: SEARCH_REQUEST,
+        payload: searchTerm,
+      });
+    }
+  }, [dispatch, searchTerm]);
+  return (
+    <div>
+      <h1>Search Result: "{searchTerm}"</h1>
+      <Row>
+        <PostCardOne posts={searchResult} />
+      </Row>
+    </div>
+  );
 };
 
 export default Search;
